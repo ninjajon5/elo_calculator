ad22cbd11e9b56e6857de17dbfeb5c10c9381540
Jon Driver <jon.a.driver.mail@gmail.com>
2026-03-07 15:18:37 +0000

    elo write working

--- elo_calculator.c
+++ elo_calculator.c
@@ -50,7 +50,7 @@ void elo_calculate_from_data( struct elo_calculator *elo, struct elo_config *con
 }
 
 void elo_write_elos( struct elo_calculator *elo, char *path ) {
-    // write_dict( &elo->elos, path ) ;
+    write_float_dict( &elo->elos, path ) ;
 }
 
 void elo_free( struct elo_calculator *elo ) {
@@ -253,21 +253,21 @@ int _elo_get_number_of_data_rows( struct elo_calculator *elo ) {
 }
 
 
-// int main( void ) {
-//     struct elo_calculator elo_calculator ;
-//     elo_init( &elo_calculator ) ;
+int main( void ) {
+    struct elo_calculator elo_calculator ;
+    elo_init( &elo_calculator ) ;
 
-//     struct elo_config elo_config = {
-//         .starting_elo = 1000.0f,
-//         .diff_factor = 400.0f,
-//         .k = 32.0f,
-//         .boost_threshold = 1.0f,
-//         .k_scaling = 1.25f
-//     } ;
+    struct elo_config elo_config = {
+        .starting_elo = 1000.0f,
+        .diff_factor = 400.0f,
+        .k = 32.0f,
+        .boost_threshold = 1.0f,
+        .k_scaling = 1.25f
+    } ;
 
-//     elo_load_data( &elo_calculator, "matches_database.csv" ) ;
-//     elo_calculate_from_data( &elo_calculator, &elo_config ) ;
-//     elo_write_elos( &elo_calculator, "elos.csv" ) ;
+    elo_load_data( &elo_calculator, "matches_database.csv" ) ;
+    elo_calculate_from_data( &elo_calculator, &elo_config ) ;
+    elo_write_elos( &elo_calculator, "elos.csv" ) ;
 
-//     elo_free( &elo_calculator ) ;
-// }
+    elo_free( &elo_calculator ) ;
+}
